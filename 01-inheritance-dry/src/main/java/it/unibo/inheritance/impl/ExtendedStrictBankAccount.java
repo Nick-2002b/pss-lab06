78c0e8a0199e448d776fd0a8cdafc8ad45b49fed
Nick-2002b <nicolae.balaban@studio.unibo.it>
2024-11-12 09:58:54 +0100

    New File: ExtendedStrictkBankAccount.java

---
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -0,0 +1,9 @@
+package it.unibo.inheritance.impl;
+
+public class ExtendedStrictBankAccount extends SimpleBankAccount {
+
+    ExtendedStrictBankAccount(final int id, final double balance){
+        super(id, balance);
+    }
+    
+}
