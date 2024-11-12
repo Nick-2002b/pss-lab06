69462d8d35818cbeef6ac62ff5cb5b4fece4f684
Nick-2002b <nicolae.balaban@studio.unibo.it>
2024-11-12 10:33:44 +0100

    Implement chargeManagementFees() in ExtendedStrickBankAccount.java

--- 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
+++ 01-inheritance-dry/src/main/java/it/unibo/inheritance/impl/ExtendedStrictBankAccount.java
@@ -1,9 +1,24 @@
 package it.unibo.inheritance.impl;
 
 public class ExtendedStrictBankAccount extends SimpleBankAccount {
+    private static final double TRANSACTION_FEE = 0.1;
 
-    ExtendedStrictBankAccount(final int id, final double balance){
+    public ExtendedStrictBankAccount(final int id, final double balance){
         super(id, balance);
     }
     
+    private boolean isWithdrawAllowed(final double amount){
+        if (amount <= getBalance()){
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void chargeManagementFees(final int id){
+        final double feeAmount = MANAGEMENT_FEE + getTransactionsCount() * ExtendedStrictBankAccount.TRANSACTION_FEE;
+        if(checkUser(id) && isWithdrawAllowed(feeAmount)){
+            withdraw(id, feeAmount);
+            resetTransactions();
+        }
+    }
 }
